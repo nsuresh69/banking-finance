a024248f498db3d542daad49e38d75669976868d
nsuresh69 <SURESHNALLA69@GMAIL.COM>
2024-10-29 17:31:24 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -8,3 +8,13 @@ resource "aws_instance" "production" {
     Name = "production"
   }
 }
+resource "aws_instance" "monitor" {
+  ami                    = "ami-0da424eb883458071"
+  key_name               = "jenkins"
+  vpc_security_group_ids = ["sg-056eedfd0afaab649"]  # Directly use the security group ID
+  instance_type          = "t2.micro"
+  count                  = 1
+  tags = {
+    Name = "production"
+  }
+}
